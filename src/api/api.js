7189f63bf049377c9ec84e75b20472904315fa7d
ALBROLSY <redamohmade505@gmail.com>
2026-07-08 20:51:15 +0300

    up 6

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 const api= axios.create({
-    baseURL:'http://127.0.0.1:8000/api',
+    // baseURL:'http://127.0.0.1:8000/api',
+    baseURL:import.meta.env.VITE_API_URL,
+    withCredentials:true,
 });
 api.interceptors.request.use(
     (config) =>{
