42afb93c6bd462e5c82566f33914369a03c36334
ALBROLSY <redamohmade505@gmail.com>
2026-07-10 16:26:58 +0300

    up 7

--- src/api/apipublic.js
+++ src/api/apipublic.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const apiPublic = axios.create({
-        baseURL:import.meta.env.VITE_API_URL,
+        baseURL:'https://alrosybackend-production.up.railway.app/api',
         
 
 
--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path";
 
-// https://vite.dev/config/
+ //https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: './',
-   define:{
-      global:'globalHtis',
-   },
-   build:{
-      commonjsOptions:{
-       transformMixedEsModules:true,
-    }
+    define:{
+       global:'globalHtis',
     },
-    optimizeDeps:{
-      include:['bootstrap']
-    },
+    build:{
+       commonjsOptions:{
+        transformMixedEsModules:true,
+     }
+     },
+     optimizeDeps:{
+       include:['bootstrap']
+     },
    resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
